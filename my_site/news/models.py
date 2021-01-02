e3ed789a9b20e48c2d40d39529f657885e7834c5
dev2033 <dev.elop.02@mail.ru>
2021-01-02 20:31:31 +0300

    add get_absolute_url and button 'read more'

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -25,6 +26,9 @@ class News(models.Model):
         verbose_name='Категория'
     )
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
+
     def __str__(self):
         return self.title
 
@@ -41,6 +45,9 @@ class Category(models.Model):
         db_index=True,
         verbose_name='Наименование категории'
     )
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"category_id": self.pk})
 
     def __str__(self):
         return self.title
