dc37dec0edcdc3f8e693c50a3438524e0e1a305c
dev2033 <dev.elop.02@mail.ru>
2021-01-02 15:31:51 +0300

    first commit

---
+++ my_site/news/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import index, get_category
+
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('category/<int:category_id>', get_category, name='category'),
+]
---
+++ my_site/news/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from .models import News, Category
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    """"""
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'category': category
+    }
+    return render(request, template_name='news/category.html', context=context)
